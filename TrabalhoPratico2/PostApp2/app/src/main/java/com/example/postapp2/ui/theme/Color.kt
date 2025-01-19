ebae0f310c08abb69d15be072ba2776453b75861
unknown <netoelpidio1@gmail.com>
2025-01-19 19:29:01 -0300

    Trabalho2

---
+++ TrabalhoPratico2/PostApp2/app/src/main/java/com/example/postapp2/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.postapp2.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF11114E)
+val PurpleGrey80 = Color(0xFF657895)
+val Pink80 = Color(0xFF87CEFA)
+
+val Purple40 = Color(0xFF8888C0)
+val PurpleGrey40 = Color(0xFF98A5B2)
+val Pink40 = Color(0xFFBFE4FF)
