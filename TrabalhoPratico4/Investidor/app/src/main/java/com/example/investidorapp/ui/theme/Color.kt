2b33a468e5baa5e1793cbb875cc0164488731c8a
unknown <netoelpidio1@gmail.com>
2025-02-02 23:05:58 -0300

    Investidor-app

---
+++ TrabalhoPratico4/Investidor/app/src/main/java/com/example/investidorapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.investidorapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFB8FFC4) // Verde claro
+val PurpleGrey80 = Color(0xFFBDE6C8) // Verde acinzentado claro
+val Pink80 = Color(0xFFC8EFB8) // Verde rosado claro
+
+val Purple40 = Color(0xFF4DA46D) // Verde médio
+val PurpleGrey40 = Color(0xFF4C7163) // Verde acinzentado médio
+val Pink40 = Color(0xFF5C7D52) // Verde rosado médio
